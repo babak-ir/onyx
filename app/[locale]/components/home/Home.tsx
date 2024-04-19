a0a91aaf459d0f75a9700700f61c8e8c67ace243
B A B A K <babakir73@gmail.com>
2024-04-19 19:08:27 +0330

    changed color

--- app/[locale]/components/home/Home.tsx
+++ app/[locale]/components/home/Home.tsx
@@ -15,7 +15,7 @@ const Home = async () => {
                     <div className="w-fit flex flex-wrap">
                         <h6 className="text-[32px] leading-[48px] font-bold text-secondary md:text-[55px] md:leading-[76px]">{t('premium')}&nbsp;</h6>
                         <div className="w-fit flex flex-col">
-                            <h6 className="text-[32px] leading-[48px] w-fit font-bold text-secondary md:text-[55px] md:leading-[76px]">{t('kaolinite')}</h6>
+                            <h6 className="text-[32px] leading-[48px] w-fit font-bold text-tertiary md:text-[55px] md:leading-[76px]">{t('kaolinite')}</h6>
                             <svg xmlns="http://www.w3.org/2000/svg" width="261" height="15" viewBox="0 0 261 15" fill="none" className="w-[147px] md:w-[252px]">
                                 <path d="M258 12C205.469 4.56608 175.404 2.58761 122.265 3.06776C75.539 3.48996 49.191 5.46327 3 12" stroke="#482019" stroke-width="6" stroke-linecap="round" />
                             </svg>
